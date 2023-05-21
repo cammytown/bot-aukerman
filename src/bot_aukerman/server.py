2b2f6d20edbf5d9ec5ca0bcb1df1ec0be97708a1
Cammy <cameronsteele@steelecameron.com>
2023-05-20 23:41:23 -0700

    start work on server

---
+++ src/bot_aukerman/server.py
@@ -0,0 +1,69 @@
+from typing import Optional
+import simpleaudio as sa
+import zmq
+import time
+
+from .performance import Performance
+
+#@TODO move to separate package?
+
+class Server:
+    socket: zmq.Socket
+    performance: Performance
+
+    # Currently playing audio object
+    active_play_obj: Optional[sa.PlayObject] = None
+
+    def __init__(self):
+        # Init ZMQ socket
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REP)
+        self.socket.bind("tcp://*:5555")
+
+    def start(self):
+        # Init Performance
+        self.performance = Performance()
+        self.performance.initialize_tts()
+        self.performance.initialize_stt()
+
+        print("bot-aukerman server started")
+
+        while(True):
+            time.sleep(0.2)
+
+            # Get any socket data (requests from other processes):
+            try:
+                message = self.socket.recv_json(flags=zmq.NOBLOCK)
+                assert isinstance(message, dict)
+                self.receive_message(message)
+            except zmq.ZMQError as e:
+                if e.errno == zmq.EAGAIN:
+                    pass
+            else:
+                #  Send reply back to client
+                reply = { "errors": None }
+                self.socket.send_json(reply)
+
+    def receive_message(self, message: dict):
+        print("server received message: ", message)
+
+        match message["type"]:
+            case "observe_user_input":
+                self.observe_user_input(message["user_input"])
+
+            case "interrupt":
+                self.interrupt()
+
+    # def stop(self):
+    #     pass
+
+    # def restart(self):
+    #     pass
+
+    def observe_user_input(self, user_input: str):
+        self.interrupt()
+        self.active_play_obj = self.performance.perform_dialogue(user_input)
+    
+    def interrupt(self):
+        if(self.active_play_obj):
+            self.active_play_obj.stop()
---
+++ tests/server-test.py
@@ -0,0 +1,41 @@
+import sys
+import subprocess
+import zmq
+from bot_aukerman.server import Server
+
+# If --server flag is set, run server
+if "--server" in sys.argv:
+    print("Running server")
+    server = Server()
+    server.start()
+
+else:
+    print("Running client")
+
+    # Run this script as a subprocess with --server flag
+    server = subprocess.Popen([sys.executable, __file__, "--server"])
+
+    try:
+        print("Connecting to server")
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://localhost:5555")
+
+        user_input = None
+        while user_input != 'q':
+            user_input = input("Enter something: ")
+            request = { "type": "observe_user_input", "user_input": user_input }
+            print("Sending request %s …" % request)
+            socket.send_json(request)
+
+            #  Get the reply
+            message = socket.recv_json()
+            print("Received reply %s [ %s ]" % (request, message))
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupted")
+        server.terminate()
+
+    except Exception as e:
+        server.terminate()
+        raise e
